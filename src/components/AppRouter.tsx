b928c416937bd98058eafa30ea5083206a1d0eb4
unknown <wkdcjfgus000@naver.com>
2023-01-03 09:49:56 +0900

    fix : 우측 친구추천에 follow 클릭 시 render가 발생해서 추천친구가 계속 바뀜
    우측 친구추천에 follow나 unfollow를 하면 실시간으로 친구 추천 리스트가
    바뀜. 그래서 안바뀌게 수정

--- src/components/AppRouter.tsx
+++ src/components/AppRouter.tsx
@@ -218,7 +218,7 @@ const AppRouter = () => {
     
             setRandomUsersProfile(filteredUsersProfile);
         }
-    }, [currentPage, usersProfile])
+    }, [currentPage])
 
     const refreshUserObj = () => {
         setUserObj(getAuth().currentUser);
